e89e003c8d08799d1b5ca8dc3bf67dbb0a43a430
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2024-01-21 11:18:57 +0530

    navigate to booking details on successful request creation

--- client/src/components/RequestApproval.tsx
+++ client/src/components/RequestApproval.tsx
@@ -1,10 +1,34 @@
 import { Button } from "@material-ui/core";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import { useBikeContext } from "../context/BikeContext";
 
 const RequestApproval = () => {
+  const { authToken } = useAuthContext();
+  const { bookingHours, selectedBike } = useBikeContext();
+
   const navigate = useNavigate();
   const handleRequestApproval = () => {
-    navigate("/booking-details");
+    axios
+      .post(
+        "http://localhost:3000/request",
+        {
+          bikeId: selectedBike._id,
+          from: bookingHours.from,
+          to: bookingHours.to,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
+      .then((requestRes) => {
+        if (requestRes.data.success) {
+          navigate("/booking-details");
+        }
+      });
   };
 
   return (
